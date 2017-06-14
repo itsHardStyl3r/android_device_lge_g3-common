950a3d61da32f7b5bb2d838aa8b0a8849d926ec5
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    g3-common: Add vndk sp libraries to system lib
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f

--- g3.mk
+++ g3.mk
@@ -253,6 +253,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.1-service.g3
 
+# VNDK-SP
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
