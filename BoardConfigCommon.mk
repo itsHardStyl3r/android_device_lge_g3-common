d4054ccef5356984c4383591c9df68f8a72f7268
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-09-28 15:01:51 +0200

    g3-common: boot permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,6 +87,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/compatibility_matrix.xml
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=g3 androidboot.wificountrycode=us user_debug=31 msm_rtb.filter=0x0
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
