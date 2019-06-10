c790a948040c307a04718d72683a2ebba74387a6
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-06-10 08:47:21 +0200

    g3-common: Build Soundtrigger HAL
    Change-Id: I144f226af52ab313d1968b32809d0f452635ccfd

--- g3.mk
+++ g3.mk
@@ -68,6 +68,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.primary.msm8974 \
     audio_policy.msm8974 \
     audio.a2dp.default \
