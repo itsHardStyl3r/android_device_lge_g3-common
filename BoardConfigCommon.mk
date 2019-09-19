2bcf2209fbe20199b1f0af9f49ebbfeefafd341d
Alessandro Astone <ales.astone@gmail.com>
2019-09-19 12:17:54 +0200

    g3-common: Add libbinder shim library
     * Commit f1cf11f1a4d5255554b49c6b299070676e53b72f of
       frameworks/native removed these support symbols
    
    Change-Id: I4fadd98c1ad11424b6ba5ff309d1e86dcf3cdca1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,3 +134,7 @@ TARGET_USE_SDCLANG := true
 #BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
 BOARD_SEPOLICY_DIRS += \
 	device/lge/g3-common/sepolicy-tmp
+
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib/libperipheral_client.so|libshim_binder.so
