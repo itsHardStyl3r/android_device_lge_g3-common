e1fbdb3547b73d7fcc9bb7dde6c839f00716ae79
Michael Bestas <mkbestas@lineageos.org>
2020-11-05 19:50:53 +0200

    g3-common: Switch to patchelf from tools-lineage
    Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

--- extract-files.sh
+++ extract-files.sh
@@ -27,7 +27,7 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib/mediadrm/libwvdrmengine.so)
-            patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
             ;;
         vendor/lib/libdsi_netctrl.so)
             sed -i 's|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g' "${2}"
