87fd151bb6945f550485f3819c18c70d234bc3aa
Bruno Martins <bgcngm@gmail.com>
2020-12-03 18:35:28 +0000

    g3-common: Configure jemalloc for low memory
    Following recommendations found in Qualcomm Android GO
    Performance Tuning Guide.
    
    Change-Id: I607344ac085dc88d5253d3627c0c6d770a97902c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,3 +125,6 @@ BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libperipheral_client.so|libshim_binder.so \
     /system/vendor/lib/libril-qc-qmi-1.so|libaudioclient_shim.so
+
+# SVELTE config
+MALLOC_SVELTE := true
