8e9387110f1ebfdc7eee7ddc7238d4056b3e3461
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    g3-common: Use threaded GLES render engine
    Change-Id: Id1feabf037b2d1648ac55aafc83c1b624beaa373

--- system_prop.mk
+++ system_prop.mk
@@ -49,7 +49,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608 \
     ro.sf.lcd_density=560 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.latch_unsignaled=1
+    debug.sf.latch_unsignaled=1 \
+    debug.renderengine.backend=threaded
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
