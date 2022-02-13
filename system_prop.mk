108c33a51454ce265fa9b4f5a8831b966fd9674e
SamarV-121 <samarvispute121@pm.me>
2022-02-13 23:16:01 +0800

    g3-common: properties: Remove obsolete debug.sf.disable_backpressure
     * It has been removed in Android S
     * https://android.googlesource.com/platform/frameworks/native/+/2ec5391acfc340c2e195d573cf5a078ff0f280d2
    
    Change-Id: I23a210a37ad257f0c8b7a3c5e1d59b2ac20cde9a

--- system_prop.mk
+++ system_prop.mk
@@ -48,7 +48,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.metadata_dynfps.disable=true \
     ro.opengles.version=196608 \
     ro.sf.lcd_density=560 \
-    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.renderengine.backend=threaded
 
