8320deaf539098ac9783b92991b2ef6bf1e12151
ZVNexus <akoskovich@pm.me>
2023-02-27 10:00:01 +0100

    g3-common: Drop support for Snapdragon Camera
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- g3.mk
+++ g3.mk
@@ -110,7 +110,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
-    Snap \
     camera.msm8974
 
 # Display
