36314b93e3a7416a56e335ac9f8d3d6bf75273f5
Edwin Moquete <edwinmmoquete@gmail.com>
2023-11-13 19:55:48 +0000

    g3-common: Add soong namespace
    Change-Id: I7b75b90e3ef3072f3af6218b0103ba5e9cf725cd

--- g3.mk
+++ g3.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Radio
 PRODUCT_PACKAGES += \
     libaudioclient_shim \
